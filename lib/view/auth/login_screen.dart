8a0b99f234b9469cfc7f1642967995678b19071d
MohammedAdel1111 <mohammedadel111111@gmail.com>
2023-04-11 14:45:11 +0200

    first commit

---
+++ lib/view/auth/login_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 50,
+          right: 20,
+          left: 20,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Welcome,',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Color.fromRGBO(0, 197, 105, 1),
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
